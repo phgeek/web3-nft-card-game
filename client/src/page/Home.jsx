f3e681d8e51a00caab5d28aafc775269559b4226
Phillippe Caetano <ph.ufeb@gmail.com>
2023-08-20 00:26:20 -0300

    Update Home page

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { PageHOC } from '../components';
+
 const Home = () => {
   return (
     <div>
@@ -10,4 +12,8 @@ const Home = () => {
   )
 };
 
-export default Home;
+export default PageHOC(
+  Home,
+  <>Welcome to Avax Gods <br /> a Web3 NFT Card Game</>,
+  <>Connect your wallet to start playing <br /> the ultimate Web3 Battle Card Game</>
+);
