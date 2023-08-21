6fde76ece45efa23950173ad90656b3f40e28225
Phillippe Caetano <ph.ufeb@gmail.com>
2023-08-21 18:57:21 -0300

    Update contract address env var

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -1,5 +1,5 @@
 import contract from './AVAXGods.json';
 
 // Deployed smart contract address
-export const ADDRESS =  '';
+export const ADDRESS =  import.meta.env.VITE_CONTRACT_ADDRESS;
 export const { abi: ABI } = contract;
