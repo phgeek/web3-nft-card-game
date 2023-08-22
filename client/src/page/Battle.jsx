0e14abdabcb70b020ff9738cd94b42dcf36c9c06
Phillippe Caetano <ph.ufeb@gmail.com>
2023-08-22 19:47:21 -0300

    Increase gas limit for  most important transactions

--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -53,7 +53,7 @@ const Battle = () => {
     playAudio(choice === 1 ? attackSound : defenseSound);
 
     try {
-      await contract.attackOrDefendChoice(choice, battleName);
+      await contract.attackOrDefendChoice(choice, battleName, { gasLimit: 200000 });
 
       setShowAlert({
         status: true,
